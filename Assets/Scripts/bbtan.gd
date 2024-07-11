1e589cafc8ea31746330b9baa2ce93ade75961e5
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-07-11 23:31:32 +0200

    Text try

--- Assets/Scripts/bbtan.gd
+++ Assets/Scripts/bbtan.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-
+var numero: int = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$AnimationPlayer.play("Move") # Replace with function body.
