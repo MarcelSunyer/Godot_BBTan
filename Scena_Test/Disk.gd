079abaefe7b986278042c42cd6de1e522245ea80
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-07-12 14:43:54 +0200

    Bastante top

--- Scena_Test/Disk.gd
+++ Scena_Test/Disk.gd
@@ -2,9 +2,18 @@ extends CharacterBody2D
 
 var ball_speed = Vector2(200,200)
 var collision_info
+var right_click = false
+var can_move = false
 
 func _physics_process(delta):
-	collision_info = move_and_collide(ball_speed*delta)
-	if collision_info:
-		ball_speed = ball_speed.bounce(collision_info.get_normal())
+	right_click = Input.is_action_pressed("Right_click")
+	
+	if right_click:
+		can_move = true
+		
+	if can_move:
+		collision_info = move_and_collide(ball_speed*delta)
+		if collision_info:
+			ball_speed = ball_speed.bounce(collision_info.get_normal())
+			can_move = false
 	
