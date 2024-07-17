1879d7995d59491ce176e47543b30909941394b5
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-07-17 11:48:35 +0200

    Collision avoid

--- Scritps/Num_shoots.gd
+++ Scritps/Num_shoots.gd
@@ -1,28 +1,12 @@
 extends Node2D
+@export var num_balls := 1
 
-
-var ball_scene = preload("res://Scena_Test/ball.tscn")
-var right_click = false
-
-# Referencia al Timer
-@onready var spawn_timer = $SpawnTimer
-@onready var ball = $Balon
-
-func _ready():
-	
-	# Configura el tiempo de espera del temporizador (en segundos)
-	spawn_timer.wait_time = 2.0  # Cambia el tiempo según sea necesario
-	# Conecta la señal "timeout" del temporizador a una función que instanciará el balón
-	spawn_timer.connect("timeout", Callable(self, "_on_SpawnTimer_timeout"))
-	# Inicia el temporizador
-	spawn_timer.start()
-	
-func _on_SpawnTimer_timeout():
+func _process(delta):
+	var ball = get_node("Balon")
 	# Duplica el nodo Ball
-	var ball_instance = ball.duplicate(5)
-	# Añadir la instancia del balón a la escena como hijo de Node2D
-	add_child(ball_instance)
- # Cambia la posición según sea necesario
-
-	# Reiniciar el temporizador si quieres instanciar balones repetidamente
-	spawn_timer.start()
+	if ball.position.y >=660:
+		for i in range(num_balls):
+			var ball_instance = ball.duplicate()
+			add_child(ball_instance)
+			ball_instance.position.x +=10 * delta
+ # Cambia la posición según sea necesari
