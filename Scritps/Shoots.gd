8827d32347820e58a3ad9135834cd4e69f1c449d
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-07-21 21:36:32 +0200

    Soc fuckdown

--- Scritps/Shoots.gd
+++ Scritps/Shoots.gd
@@ -5,14 +5,15 @@ var position_clicked = Vector2()
 
 var y_limit = 800
 
+var collision_info
+
 var left_click = false
+var right_click = false
 var can_move = false
 var moving = false
 
-@export var balls = 5  # Número de bolas en la ráfaga
-@export var interval_vall_spawn = 0.1 
-
-var direction
+var burst_count = 5  # Número de bolas en la ráfaga
+var burst_interval = 0.1 
 
 func _physics_process(delta):
 	left_click = Input.is_action_just_pressed("Left_click")
@@ -24,25 +25,34 @@ func _physics_process(delta):
 	if !moving: 
 		if left_click:
 			if position_clicked.y < y_limit:
-				start_burst()
+				var direction = (position_clicked - global_position).normalized()
+				ball_speed = direction * 700
 				can_move = true
-				moving = true
+				moving =true
+				start_burst()
+				shoot_ball(direction)
+			
+	
+	if can_move:
+		collision_info = move_and_collide(ball_speed * delta)
+		if collision_info:
+			ball_speed = ball_speed.bounce(collision_info.get_normal())
 
 func start_burst():
-	direction = (position_clicked - global_position).normalized()
+	var direction = (position_clicked - global_position).normalized()
 	ball_speed = direction * 700
-	for i in range(balls):
+	# Inicia la ráfaga de disparos
+	await get_tree().create_timer(burst_interval).timeout
+	for i in range(burst_count):
+		if not can_move:
+			break
 		shoot_ball(direction)
-		await get_tree().create_timer(interval_vall_spawn).timeout
+		await get_tree().create_timer(burst_interval).timeout
 		
 func shoot_ball(direction):
 	# Crear una nueva instancia de la bola aquí (asegúrate de tener una escena de bola preparada)
-	var ball_instance = preload("res://Scenas/Ball.tscn").instantiate()  # Reemplaza con la ruta correcta a tu escena de bola
-	ball_instance.position = Vector2(0,0)  # Posición local de la bola en el emisor
-	ball_instance.ball_speed = direction * 700
+	var ball_instance = preload("res://Scenes/Ball.tscn").instantiate()  # Reemplaza con la ruta correcta a tu escena de bola
+	ball_instance.position = Vector2(0,0)
 	print_debug(ball_instance.position)
-	
-	# Ajustar la posición global de la bola
-	ball_instance.global_position = global_position + ball_instance.position
-	
+	ball_instance.ball_speed = direction * 700
 	get_parent().add_child(ball_instance)
