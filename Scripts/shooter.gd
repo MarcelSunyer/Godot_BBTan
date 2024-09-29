e8099f4bf10ab32776e13bb65050618d23157d9e
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-09-29 15:59:52 +0200

    Disparo + timer

--- Scripts/shooter.gd
+++ Scripts/shooter.gd
@@ -1,10 +1,16 @@
 extends Node2D
 
 @onready var main = get_tree().get_root().get_node("main")
-@onready var projectile_scene = preload("res://projectile.tscn")
+@onready var projectile_scene = preload("res://Scenes/projectile.tscn")
+@onready var cooldown_timer = $Cooldown
 
 @export var max_shoots = 5
 var current_shots = 0
+
+	
+func _process(delta):
+	if Input.is_action_just_pressed("Left_click"):
+		cooldown_timer.start()
 
 # Shoots a projectile
 func shoot():
@@ -15,6 +21,6 @@ func shoot():
 	main.add_child.call_deferred(instance)
 
 func _on_cooldown_timeout():
-	if(current_shots < max_shoots):
+	if(current_shots <= max_shoots):
 		shoot()
 		current_shots += 1
