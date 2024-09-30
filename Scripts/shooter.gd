b7de220fbe02eb71f85ce79b1ef091b6567ce8ab
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-09-30 19:53:05 +0200

    aIM UPDATE BONA

--- Scripts/shooter.gd
+++ Scripts/shooter.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var main = get_tree().get_root().get_node("main")
 @onready var projectile_scene = preload("res://Scenes/projectile.tscn")
 @onready var cooldown_timer = $Cooldown
+@onready var aim = $aim
 
 @export var max_shoots = 5
 var current_shots = 0
@@ -11,8 +12,7 @@ var current_shots = 0
 func _process(delta):
 	if Input.is_action_just_pressed("Left_click"):
 		if get_global_mouse_position().y <= 750:
-			cooldown_timer.start()
-	print_debug(get_global_mouse_position().y)	
+			cooldown_timer.start()	
 
 func shoot():
 		var instance = projectile_scene.instantiate()
