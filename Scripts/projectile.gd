32310f3eeb4db52b44c309a6ca9df3a448f774fe
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2024-11-04 18:51:27 +0100

    Detección de bloques

--- Scripts/projectile.gd
+++ Scripts/projectile.gd
@@ -1,34 +1,35 @@
 extends CharacterBody2D
 
 @export var speed = 700
-@export var reset_layer = 4  # Capa en la que se destruirá el proyectil
-@export var block_layer = 5  # Capa en la que se destruirá el proyectil
+@export var reset_layer = 4  
+@export var block_layer = "block_layer"
 
 var spawnPos: Vector2
 var spawnRot: float
 var vel: Vector2
 
-var main_node  # Referencia al nodo principal que gestiona los disparos
+var main_node
 
 func _ready():
-	global_position = spawnPos  # Colocar en la posición de inicio
-	global_rotation = spawnRot  # Aplicar la rotación inicial
+	global_position = spawnPos 
+	global_rotation = spawnRot 
 
 func _physics_process(delta):
-	var collision_info = move_and_collide(vel * delta)  # Mover el proyectil
+	var collision_info = move_and_collide(vel * delta) 
 
 	if collision_info:
 		var collider = collision_info.get_collider()
-		if collider.collision_layer == block_layer:
-			collider.reduce_vida()  # Llamar a la función para reducir la vida
+
+		if collider.is_in_group("bloque"):
+			collider.reduce_vida()
 			
 		if collider.collision_layer == reset_layer:
-			remove_from_balls()  # Eliminar de la lista del nodo principal
+			remove_from_balls() 
 			queue_free()
 		else:
-			vel = vel.bounce(collision_info.get_normal())  # Rebote en colisión
+			vel = vel.bounce(collision_info.get_normal())
 
 func remove_from_balls():
 	if main_node and self in main_node.balls:
-		main_node.balls.erase(self)  # Eliminar de la lista de proyectiles
-		main_node.move_to_ball_position(global_position)  # Notificar al shooter
+		main_node.balls.erase(self) 
+		main_node.move_to_ball_position(global_position)
