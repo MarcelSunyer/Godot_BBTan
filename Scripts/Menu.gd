8044fe94bf0e0378aebbe9c1c282d20ffa439796
MarcelSunyer <45765502+MarcelSunyer@users.noreply.github.com>
2025-02-27 17:00:31 +0100

    Navegation

---
+++ Scripts/Menu.gd
@@ -0,0 +1,10 @@
+extends Node
+
+func _on_start_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/main.tscn")
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
+	
+func _on_exit_pressed() -> void:
+	get_tree().quit
